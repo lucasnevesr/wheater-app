63da14d80d5b139bda6f46bc059ba8090b4b1707
Lucas Neves <nevessricardoo@gmail.com>
2024-06-05 21:51:39 -0300

    Initial config

---
+++ src/cities.js
@@ -0,0 +1,167 @@
+export default [
+  {
+    Key: "28143",
+    LocalizedName: "Daca",
+    EnglishName: "Dhaka",
+    Country: {
+      ID: "BD",
+      LocalizedName: "Bangladesh",
+      EnglishName: "Bangladesh",
+    },
+    TimeZone: {
+      Code: "BDT",
+      Name: "Asia/Dhaka",
+      GmtOffset: 6.0,
+      IsDaylightSaving: false,
+      NextOffsetChange: null,
+    },
+    GeoPosition: {
+      Latitude: 23.7098,
+      Longitude: 90.40711,
+      Elevation: {
+        Metric: {
+          Value: 5.0,
+          Unit: "m",
+          UnitType: 5,
+        },
+        Imperial: {
+          Value: 16.0,
+          Unit: "ft",
+          UnitType: 0,
+        },
+      },
+    },
+    LocalObservationDateTime: "2024-06-06T06:13:00+06:00",
+    EpochTime: 1717632780,
+    WeatherText: "Sol entre nuvens",
+    WeatherIcon: 4,
+    HasPrecipitation: false,
+    PrecipitationType: null,
+    IsDayTime: true,
+    Temperature: {
+      Metric: {
+        Value: 28.2,
+        Unit: "C",
+        UnitType: 17,
+      },
+      Imperial: {
+        Value: 83.0,
+        Unit: "F",
+        UnitType: 18,
+      },
+    },
+    MobileLink:
+      "http://www.accuweather.com/pt/bd/dhaka/28143/current-weather/28143?lang=pt-br",
+    Link: "http://www.accuweather.com/pt/bd/dhaka/28143/current-weather/28143?lang=pt-br",
+  },
+  {
+    Key: "113487",
+    LocalizedName: "Kinshasa",
+    EnglishName: "Kinshasa",
+    Country: {
+      ID: "CD",
+      LocalizedName: "República Democrática do Congo",
+      EnglishName: "Democratic Republic of the Congo",
+    },
+    TimeZone: {
+      Code: "WAT",
+      Name: "Africa/Kinshasa",
+      GmtOffset: 1.0,
+      IsDaylightSaving: false,
+      NextOffsetChange: null,
+    },
+    GeoPosition: {
+      Latitude: -4.31642,
+      Longitude: 15.29834,
+      Elevation: {
+        Metric: {
+          Value: 180.0,
+          Unit: "m",
+          UnitType: 5,
+        },
+        Imperial: {
+          Value: 590.0,
+          Unit: "ft",
+          UnitType: 0,
+        },
+      },
+    },
+    LocalObservationDateTime: "2024-06-06T01:11:00+01:00",
+    EpochTime: 1717632660,
+    WeatherText: "Algumas nuvens",
+    WeatherIcon: 36,
+    HasPrecipitation: false,
+    PrecipitationType: null,
+    IsDayTime: false,
+    Temperature: {
+      Metric: {
+        Value: 24.6,
+        Unit: "C",
+        UnitType: 17,
+      },
+      Imperial: {
+        Value: 76.0,
+        Unit: "F",
+        UnitType: 18,
+      },
+    },
+    MobileLink:
+      "http://www.accuweather.com/pt/cd/kinshasa/113487/current-weather/113487?lang=pt-br",
+    Link: "http://www.accuweather.com/pt/cd/kinshasa/113487/current-weather/113487?lang=pt-br",
+  },
+  {
+    Key: "60449",
+    LocalizedName: "Santiago",
+    EnglishName: "Santiago",
+    Country: {
+      ID: "CL",
+      LocalizedName: "Chile",
+      EnglishName: "Chile",
+    },
+    TimeZone: {
+      Code: "CLT",
+      Name: "America/Santiago",
+      GmtOffset: -4.0,
+      IsDaylightSaving: false,
+      NextOffsetChange: "2024-09-08T04:00:00Z",
+    },
+    GeoPosition: {
+      Latitude: -33.44643,
+      Longitude: -70.65901,
+      Elevation: {
+        Metric: {
+          Value: 522.0,
+          Unit: "m",
+          UnitType: 5,
+        },
+        Imperial: {
+          Value: 1712.0,
+          Unit: "ft",
+          UnitType: 0,
+        },
+      },
+    },
+    LocalObservationDateTime: "2024-06-05T20:10:00-04:00",
+    EpochTime: 1717632600,
+    WeatherText: "Nublado",
+    WeatherIcon: 7,
+    HasPrecipitation: false,
+    PrecipitationType: null,
+    IsDayTime: false,
+    Temperature: {
+      Metric: {
+        Value: 10.9,
+        Unit: "C",
+        UnitType: 17,
+      },
+      Imperial: {
+        Value: 52.0,
+        Unit: "F",
+        UnitType: 18,
+      },
+    },
+    MobileLink:
+      "http://www.accuweather.com/pt/cl/santiago/60449/current-weather/60449?lang=pt-br",
+    Link: "http://www.accuweather.com/pt/cl/santiago/60449/current-weather/60449?lang=pt-br",
+  },
+];
